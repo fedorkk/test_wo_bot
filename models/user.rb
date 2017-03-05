321bd2ce31b133fb2fe60711558da2fefcc4d8e8
Fedor Koshel <fedorkk@gmail.com>
2017-03-05 16:09:36 +0300

    Small fix for old ruby versions

--- models/user.rb
+++ models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   has_many :trainings
 
   def current_training
-    return trainings.not_completed.last if  trainings.not_completed.last
-    training = trainings.completed.last&.dup || trainings.new
+    return trainings.not_completed.last if trainings.not_completed.last
+    training = (trainings.completed.last ? trainings.completed.last.dup : nil) || trainings.new
     training.assign_attributes(completed_rounds: 0)
     training
   end
